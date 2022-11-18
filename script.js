9fcf99a1e14287bf12ef2446251861b3998d15d9
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-11-18 17:51:01 +0200

    Edit index.html styles.css

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener('keydown', function (event) {
+    const audio = document.querySelector(`audio[data-key="${event.code}"]`);
+    if (!audio) return;
+    audio.play()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
